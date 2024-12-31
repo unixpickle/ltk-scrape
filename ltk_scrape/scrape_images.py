c055a6afb10cbc148dfec72476c837be6ab3d825
Alex Nichol <unixpickle@gmail.com>
2024-12-31 16:26:04 -0500

    parallel image download

--- ltk_scrape/scrape_images.py
+++ ltk_scrape/scrape_images.py
@@ -1,6 +1,10 @@
 import argparse
 import io
+from multiprocessing import Pool, Process, Queue
+import sys
 import time
+import traceback
+from typing import Any, Optional, Tuple
 
 import requests
 from PIL import Image
@@ -16,17 +20,25 @@ def main():
     )
     parser.add_argument("--only_with_price", action="store_true")
     parser.add_argument("--proxy", type=str, default=None)
+    parser.add_argument("--concurrency", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=10000)
     args = parser.parse_args()
 
     db = DB(args.db_path)
 
     proxies = None if args.proxy is None else {"http": args.proxy, "https": args.proxy}
+    req_queue = Queue()
+    resp_queue = Queue(maxsize=args.concurrency)
+    fetchers = [
+        Fetcher(proxies=proxies, req_queue=req_queue, resp_queue=resp_queue)
+        for _ in range(args.concurrency)
+    ]
 
-    with requests.Session() as sess:
+    try:
         while True:
             t1 = time.time()
             unvisited = db.missing_images(
-                args.image_type, 10000, only_with_price=args.only_with_price
+                args.image_type, args.batch_size, only_with_price=args.only_with_price
             )
             t2 = time.time()
             print("took", t2 - t1, "seconds to find unvisited images")
@@ -34,7 +46,43 @@ def main():
                 print("no more remaining images to download")
                 break
             for id, url in unvisited:
-                print(f"fetching: {id} ...")
+                req_queue.put((id, url))
+            for _ in range(len(unvisited)):
+                id, data, err = resp_queue.get()
+                if data is None:
+                    print(f"error for {id}: {err}")
+                    db.insert_image(args.image_type, id, blob=None, error=err)
+                else:
+                    print(f"fetched {id}")
+                    db.insert_image(args.image_type, id, blob=data)
+    finally:
+        for fetcher in fetchers:
+            fetcher.kill()
+
+
+class Fetcher:
+    def __init__(
+        self, proxies: Any, req_queue: Queue, resp_queue: Queue, *args, **kwargs
+    ):
+        self.client_args = args
+        self.client_kwargs = kwargs
+        self.proc = Process(
+            target=Fetcher._worker,
+            args=(proxies, req_queue, resp_queue),
+            name="fetcher-worker",
+            daemon=True,
+        )
+        self.proc.start()
+
+    def kill(self):
+        self.proc.kill()
+        self.proc.join()
+
+    @staticmethod
+    def _worker(proxies, req_queue, resp_queue):
+        with requests.Session() as sess:
+            while True:
+                id, url = req_queue.get()
                 try:
                     result_image = sess.get(
                         url, stream=True, timeout=5, proxies=proxies
@@ -42,17 +90,17 @@ def main():
                     # Make sure the image is actually valid.
                     Image.open(io.BytesIO(result_image)).load()
                 except KeyboardInterrupt:
-                    raise
+                    traceback.print_exc()
+                    sys.exit(1)
                 except requests.exceptions.ReadTimeout as exc:
-                    db.insert_image(args.image_type, id, blob=None, error=str(exc))
+                    resp_queue.put((id, None, str(exc)))
                     continue
                 except Exception as exc:
                     if "SOCKSHTTP" in str(exc):
                         time.sleep(1.0)
-                    print("error", exc)
-                    db.insert_image(args.image_type, id, blob=None, error=str(exc))
+                    resp_queue.put((id, None, str(exc)))
                     continue
-                db.insert_image(args.image_type, id, blob=result_image)
+                resp_queue.put((id, result_image, None))
 
 
 if __name__ == "__main__":
